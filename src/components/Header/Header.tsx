50bd071d2b1c193ba623a58414ea368c16201477
Paola de Oliveira <paola9liveira06@gmail.com>
2025-06-02 20:41:38 -0300

    pequenas alterações

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,8 +3,6 @@ import styled from 'styled-components';
 
 const HeaderContainer = styled.div`
     display: flex;
-    align-items: center;
-    padding-top: 12rem;
     padding-left: 5rem;
     gap: 10rem;
 `;
@@ -12,33 +10,47 @@ const HeaderContainer = styled.div`
 const HeaderSocial = styled.div`
     display: flex;
     flex-direction: column;
-    gap: 30px;
+    gap: 25px;
+    padding-top: 9rem;
 
-    img {width: 30px;}
+    img {width: 25px;}
 `;
+
+const HeaderInfo = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+`
 
 const HeaderTitle = styled.h1`
     font-size: 2.5rem;
-    text-align: center;
+    padding-top: 5rem;
 `;
 
 
 const Headerp = styled.p``;
+
+const HeaderButton = styled.button`
+
+`
 
 const Header = () => {
   return (
     <div>
       <HeaderContainer>
         <HeaderSocial>
-          <img src="https://img.icons8.com/?size=100&id=DpOQ6G5p47f0&format=png&color=000000" />
-          <img src="https://img.icons8.com/?size=100&id=BRElTmOLC6Bh&format=png&color=000000" />
-          <img src="https://img.icons8.com/?size=100&id=12598&format=png&color=000000" />
+          <a href=""><img src="https://img.icons8.com/?size=100&id=DpOQ6G5p47f0&format=png&color=000000" /></a>
+          <a href=""><img src="https://img.icons8.com/?size=100&id=BRElTmOLC6Bh&format=png&color=000000" /></a>
+          <a href=""><img src="https://img.icons8.com/?size=100&id=12598&format=png&color=000000" /></a>
         </HeaderSocial>
-
+        
+        <HeaderInfo>
         <HeaderTitle>Paola de Oliveira 🖐</HeaderTitle>
-      </HeaderContainer>
+        <Headerp>oi</Headerp>
+        <HeaderButton><button><a href="">Say hello!</a></button></HeaderButton>
+        </HeaderInfo>
 
-      <Headerp></Headerp>
+      </HeaderContainer>
     </div>
   );
 };
