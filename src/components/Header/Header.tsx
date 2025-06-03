80b28a73803cf43c76dd1ad55d90f839f4c09a7c
Paola de Oliveira <paola9liveira06@gmail.com>
2025-06-02 21:53:13 -0300

    titulo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,20 +1,24 @@
-
 import styled from 'styled-components';
 
+/*comentários de ajuda*/ 
 
 const HeaderContainer = styled.div`
     display: flex;
     padding-left: 5rem;
     gap: 10rem;
+    justify-content: center;
+    align-items: center;
 `;
-
-
 
 const HeaderSocial = styled.div`
     display: flex;
     flex-direction: column;
     gap: 25px;
-    padding-top: 9rem;
+    position: fixed;/* Fixa os ícones na tela, eles não se movem com o scroll */
+    top: 50%;                    /* Move o topo do elemento até 50% da altura da tela (meio vertical) */
+    left: 4rem;                  /* Mantém o elemento afastado 2rem da lateral esquerda da tela */
+    transform: translateY(-50%); /* Move o elemento para cima em 50% da sua própria altura, 
+                                    para que ele fique exatamente centralizado verticalmente */
 
     img {width: 25px;}
 `;
@@ -22,13 +26,14 @@ const HeaderSocial = styled.div`
 const HeaderInfo = styled.div`
     display: flex;
     flex-direction: column;
-    gap: 1rem;
+    gap: 2rem;
     text-align: center;
+    
 `
 
 const HeaderTitle = styled.h1`
     font-size: 2.5rem;
-    padding-top: 6.5rem;
+    padding-top:6rem; 
 `;
 
 
@@ -53,7 +58,6 @@ const Header = () => {
         <Headerp>oi</Headerp>
         <HeaderButton>Say hello!</HeaderButton>
         </HeaderInfo>
-
 
       </HeaderContainer>
     </div>
